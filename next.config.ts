14911705e3d744f3c3dcc8966942f9c1b0fbd11e
Kai Chu <kai.chu15@gmail.com>
2025-09-07 20:21:24 -0700

    feat: added cover

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 // Comment out all of nextConfig, if you want to run locally with npm run dev
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '',
-  assetPrefix: 'https://kaichuuu.github.io',
+  // output: 'export',
+  // basePath: '',
+  // assetPrefix: 'https://kaichuuu.github.io',
 };
 
 export default nextConfig;
